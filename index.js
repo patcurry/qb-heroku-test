88dde27aaa268adf5093777f4af8180b651c6fad
currypat <currypat1985@gmail.com>
2018-08-26 12:10:46 +0200

    not understanding why this does not work

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello Maggie!'))
+app.get('/', (req, res) => res.send('Hello Maggie!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
